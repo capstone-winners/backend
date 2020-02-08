7146852d5aea3aa7456fce17b8f97fb81def415e
Jason Booth <booth.j@husky.neu.edu>
2020-02-08 13:45:18 -0500

    Add mqtt device publish

--- src/Service.js
+++ src/Service.js
@@ -4,13 +4,20 @@ const mqtt = require('./MQTT')
 
 function handleAction(type, id, action) {
     if(validate(type, id, action)) {
-        return type + id + "\n" + action.foo
+        message = {
+            id,
+            action
+        }
+        mqtt.device.publish(type, message)
+        return type + " " + id + "\n" + action.foo
     } else {
         return "Error!"
     }
 }
 
 function validate(type, id, action) {
+    // Read from database to see if action is valid
+    // Check if device is alive (read from DB)
     return true
 }
 
