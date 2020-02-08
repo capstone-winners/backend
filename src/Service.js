bab9ee0d723ed5f85ee935e68af61113aec618b4
Jason Booth <booth.j@husky.neu.edu>
2020-02-08 18:29:12 -0500

    Fix error handle for action

--- src/Service.js
+++ src/Service.js
@@ -3,16 +3,15 @@ const mqtt = require('./MQTT')
 // mqtt.device.publish('topic_1', JSON.stringify({ test_data: 1}));
 
 function handleAction(type, deviceId, action) {
-    if(validate(type, deviceId, action)) {
-        message = {
-            deviceId,
-            ...action
-        }
-        mqtt.device.publish(type, JSON.stringify(message))
-        return type + " " + deviceId + "\n" + JSON.stringify(action)
-    } else {
-        return "Error!"
+    if(!validate(type, deviceId, action)) {
+        throw "Error, invalid action for device type"
     }
+    message = {
+        deviceId,
+        ...action
+    }
+    mqtt.device.publish(type, JSON.stringify(message))
+    return type + "\n" + JSON.stringify(message)
 }
 
 function validate(type, id, action) {
