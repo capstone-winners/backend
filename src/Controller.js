dcd9792805dd2b2944c898e9b37a85536b20df83
Jason Booth <booth.j@husky.neu.edu>
2020-03-29 22:37:39 -0400

    Add security token check

--- src/Controller.js
+++ src/Controller.js
@@ -1,11 +1,13 @@
 const service = require('./Service')
 
+const auth = "Authorization"
+
 function setup(app) {
     app.get('/', (req, res) => res.send('Hello World!'))
     app.post('/action/:type/:id', (req, res) => {
         console.log(req.params.type + " " + req.params.id)
         console.log(req.body)
-        res.send(service.handleAction(req.params.type, req.params.id, req.body))
+        res.send(service.handleAction(req.get(auth), req.params.type, req.params.id, req.body))
     })
 };
 
--- src/Service.js
+++ src/Service.js
@@ -1,6 +1,9 @@
 const mqtt = require('./MQTT')
 
-function handleAction(type, deviceId, action) {
+function handleAction(token, type, deviceId, action) {
+    if(!checkToken(token)) {
+        throw "Error, invalid token"
+    }
     if(!validate(type, deviceId, action)) {
         throw "Error, invalid action for device type"
     }
@@ -16,6 +19,13 @@ function validate(type, id, action) {
     // Read from database to see if action is valid
     // Check if device is alive (read from DB)
     return true
+}
+
+function checkToken(token) {
+    if (token === "token") {
+        return true
+    }
+    return false
 }
 
 module.exports = {handleAction}
